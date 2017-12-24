a9becc36554405715ce7057ebeba1bd856a9cc1e
DINHTHICHANG <dinhthichang2397@gmail.com>
2017-12-24 22:24:14 +0700

    Work2:PhieuThueFormView-UI

---
+++ LTUD-FinalTermProject/PhieuThueFormView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LTUD_FinalTermProject
+{
+    public partial class PhieuThueFormView : Form
+    {
+        public PhieuThueFormView()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
