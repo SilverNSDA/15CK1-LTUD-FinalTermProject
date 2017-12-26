5d6317b3a261c98048abb07aab80dedb9fcfd2f4
DINHTHICHANG <dinhthichang2397@gmail.com>
2017-12-26 08:17:26 +0700

    Mô tả công việc đã làm+Work3 (chưa hoàn thành)

---
+++ LTUD-FinalTermProject/TaoPhieuThueFormView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LTUD_FinalTermProject
+{
+    public partial class TaoPhieuThueFormView : Form
+    {
+        public TaoPhieuThueFormView()
+        {
+            InitializeComponent();
+        }
+    }
+}
