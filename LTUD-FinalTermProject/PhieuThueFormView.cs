10872588537754f2e8e1f5a5007672fab472c531
NguyenGiaBao1560036 <34787016+NguyenGiaBao1560036@users.noreply.github.com>
2018-01-13 22:02:29 +0700

    Data Gridview

--- LTUD-FinalTermProject/PhieuThueFormView.cs
+++ LTUD-FinalTermProject/PhieuThueFormView.cs
@@ -14,9 +14,38 @@ namespace LTUD_FinalTermProject
     {
         public PhieuThueFormView()
         {
+            dgvCTPhieuThue.Columns[0].Name = "ID";
+            dgvCTPhieuThue.Columns[1].Name = "MaPhieuThue";
+            dgvCTPhieuThue.Columns[2].Name = "MaKhachHang";
+
+            dgvPhieuThue.Columns[0].Name = "ID";
+            dgvPhieuThue.Columns[0].Name = "MaPhong";
+            dgvPhieuThue.Columns[0].Name = "NgayBatDau";
+            xuatpt();
+            
             InitializeComponent();
         }
+        public void xuatpt()
+        {
+            DataGridViewRow row = dgvCTPhieuThue.CurrentRow;
+            var kh = DataUtil.DSCT_PhieuThue();
+            foreach (var khh in kh)
+            {
+                row.Cells["ID"].Value = khh.ID;
+                row.Cells["MaPhieuThue"].Value = khh.MaPhieuThue;
+                row.Cells["MaKhachHang"].Value = khh.MaKhachHang;
+               
+            }
+            DataGridViewRow row1 = dgvCTPhieuThue.CurrentRow;
+            var khh1 = DataUtil.DSPhieuThue();
+            foreach (var khh in khh1)
+            {
+                row.Cells["ID"].Value = khh.ID;
+                row.Cells["MaPhong"].Value = khh.MaPhong;
+                row.Cells["NgayBatDau"].Value = khh.NgayBatDau;
 
+            }
+        }
         private void label5_Click(object sender, EventArgs e)
         {
 
--- LTUD-FinalTermProject/TaoPhieuThueFormView.cs
+++ LTUD-FinalTermProject/TaoPhieuThueFormView.cs
@@ -14,7 +14,26 @@ namespace LTUD_FinalTermProject
     {
         public TaoPhieuThueFormView()
         {
+            dgvKH.Columns[0].Name = "ID";
+            dgvKH.Columns[1].Name = "HoTen";
+            dgvKH.Columns[2].Name = "DiaChi";
+            dgvKH.Columns[3].Name = "LoaiKH";
+            dgvKH.Columns[4].Name = "CMND";
+            xuat();
             InitializeComponent();
+        }
+        public void xuat()
+        {
+            DataGridViewRow row = dgvKH.CurrentRow;
+            var kh = DataUtil.DSKhachHang();
+            foreach (var khh in kh)
+            {
+                row.Cells["ID"].Value = khh.ID;
+                row.Cells["HoTen"].Value = khh.HoTen;
+                row.Cells["DiaChi"].Value = khh.DiaChi;
+                row.Cells["LoaiKH"].Value = khh.LoaiKhachHang;
+                row.Cells["CMND"].Value = khh.CMND;
+            }
         }
     }
 }
