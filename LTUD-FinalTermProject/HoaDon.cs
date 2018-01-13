7ad50920afb4be93510f02713cb554c3b3cce17c
SilverNSDA <HatoriNSDA@gmail.com>
2018-01-13 11:11:35 +0700

    update query, add part of datautil
    delete unesscary query
    edit some table query
    edit some sampledata
    add table manage cs class

---
+++ LTUD-FinalTermProject/HoaDon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace LTUD_FinalTermProject
+{
+    class HoaDon
+    {
+        public string MaPhieuThue { get; set; }
+        public string MaKhachHang { get; set; }
+        public DateTime NgayThanhToan { get; set; }
+        public static int iMaPhieuThue = 0, iMaKhachHang = 1, iNgayThanhToan = 2;
+
+        public static HoaDon Read(SqlDataReader reader)
+        {
+            var ctpt = new HoaDon()
+            {
+                MaPhieuThue = reader.GetString(iMaPhieuThue),
+                MaKhachHang = reader.GetString(iMaKhachHang),
+                NgayThanhToan=reader.GetDateTime(iNgayThanhToan)
+            };
+            return ctpt;
+        }
+    }
+}
---
+++ LTUD-FinalTermProject/MD_SuDungPhong.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace LTUD_FinalTermProject
+{
+    class MD_SuDungPhong
+    {
+        public string ID { get; set; }
+        public string MaPhong { get; set; }
+        public DateTime NgayBatDau { get; set; }
+        public DateTime NgayKetThuc { get; set; }
+        public int MatDo { get; set; }
+        public static int iID = 0, iMaPhong = 1, iNgayBatDau = 2, iNgayKetThuc = 3, iMatDo = 4;
+
+        public static MD_SuDungPhong Read(SqlDataReader reader)
+        {
+            var bc = new MD_SuDungPhong()
+            {
+                ID = reader.GetString(iID),
+                MaPhong = reader.GetString(iMaPhong),
+                NgayBatDau = reader.GetDateTime(iNgayBatDau),
+                NgayKetThuc = reader.GetDateTime(iNgayKetThuc),
+                MatDo = reader.GetInt32(iMatDo)
+            };
+            return bc;
+        }
+    }
+}
